9c631effc2fb7b21a996775bfbe7b244e5fe059b
li w <wei.li@discovery.wang>
2023-05-04 19:09:08 +0800

    feat: 修改请求方式

--- src/pages/layout/editor/index.jsx
+++ src/pages/layout/editor/index.jsx
@@ -1,37 +1,43 @@
 import styles from './index.module.less'
-import {useState} from "react"
+import {useRef, useState} from "react"
 import axios from "axios"
 
 const Editor = () => {
 	const [searchWord, setSearchWord] = useState('')
+	const isComposed = useRef(false)
 
 	const fnChange = e => {
 		setSearchWord(e.target.value)
 	}
 
 	const fnSend = (e) => {
+		if (isComposed.current) {
+			return
+		}
 		if (e.keyCode === 13) {
+			e.preventDefault()
+
 			// 回车键被按下
-			// axios({
-			// 	method: 'POST',
-			// 	url: 'https://api.openai.com/v1/chat/completions',
-			// 	headers: {
-			// 		Authorization: 'Bearer ' + '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',
-			// 	},
-			// 	data: {
-			// 		model: "gpt-3.5-turbo",
-			// 		messages: [{role: "user", "content": searchWord}],
-			// 		temperature: 0.7
-			// 	}
-			// })
 			axios({
-				method: 'get',
-				url: 'http://101.42.1.176',
+				method: 'POST',
+				url: 'https://api.openai.com/v1/chat/completions',
+				headers: {
+					Authorization: 'Bearer ' + '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',
+				},
 				data: {
-					email: 'wei.li@discovery.wang',
-					pass:  '123456'
+					model: "gpt-3.5-turbo",
+					messages: [{role: "user", "content": searchWord}],
+					temperature: 0.7
 				}
 			})
+			// axios({
+			// 	method: 'POST',
+			// 	url: 'http://101.42.1.176/admin',
+			// 	data: {
+			// 		email: 'wei.li@discovery.wang',
+			// 		pass:  '123456'
+			// 	}
+			// })
 			// fetch('http://101.42.1.176/', {
 			// 	method: 'POST',
 			// 	mode: 'cors',
@@ -47,6 +53,11 @@ const Editor = () => {
 		}
 	}
 
+	// 输入法中文状态改变
+	const fnCompositionChange = (flag) => {
+		isComposed.current = flag
+	}
+
 	return (
 		<div className={styles.editor}>
 			<div className={styles.stopBtn}>
@@ -57,7 +68,9 @@ const Editor = () => {
 				type="text"
 				placeholder="Input Something"
 				onChange={fnChange}
-				onKeyUp={fnSend}
+				onKeyDown={fnSend}
+				onCompositionStart={() => fnCompositionChange(true)}
+				onCompositionEnd={() => fnCompositionChange(false)}
 			/>
 		</div>
 	)
