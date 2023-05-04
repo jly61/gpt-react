10079d304798704f8b60b6e770d679010200823b
li w <wei.li@discovery.wang>
2023-05-04 19:40:56 +0800

    feat: 修改apikey

--- src/pages/layout/editor/index.jsx
+++ src/pages/layout/editor/index.jsx
@@ -18,39 +18,39 @@ const Editor = () => {
 			e.preventDefault()
 
 			// 回车键被按下
-			// axios({
-			// 	method: 'POST',
-			// 	url: 'https://api.openai.com/v1/chat/completions',
-			// 	headers: {
-			// 		// Authorization: 'Bearer ' + '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',
-			// 		Authorization: 'Bearer ' + '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',
-			// 	},
-			// 	data: {
-			// 		model: "gpt-3.5-turbo",
-			// 		messages: [{role: "user", "content": searchWord}],
-			// 		temperature: 0.7
-			// 	}
-			// })
 			axios({
 				method: 'POST',
-				url: 'http://101.42.1.176/admin',
+				url: 'https://api.openai.com/v1/chat/completions',
+				headers: {
+					Authorization: 'Bearer ' + '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',
+					// Authorization: 'Bearer ' + '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',
+				},
 				data: {
-					email: 'wei.li@discovery.wang',
-					pass:  '123456'
+					model: "gpt-3.5-turbo",
+					messages: [{role: "user", "content": searchWord}],
+					temperature: 0.7
 				}
 			})
-			fetch('http://101.42.1.176/', {
-				method: 'POST',
-				mode: 'cors',
-				headers: {
-					"Content-type": "application/json; charset=UTF-8",
-				},
-				body: JSON.stringify({
-					email: 'wei.li@discovery.wang',
-					pass:  '123456'
-				}),
-				credentials: "include"
-			}).then(res => {})
+			// axios({
+			// 	method: 'POST',
+			// 	url: 'http://101.42.1.176/admin',
+			// 	data: {
+			// 		email: 'wei.li@discovery.wang',
+			// 		pass:  '123456'
+			// 	}
+			// })
+			// fetch('http://101.42.1.176/', {
+			// 	method: 'POST',
+			// 	mode: 'cors',
+			// 	headers: {
+			// 		"Content-type": "application/json; charset=UTF-8",
+			// 	},
+			// 	body: JSON.stringify({
+			// 		email: 'wei.li@discovery.wang',
+			// 		pass:  '123456'
+			// 	}),
+			// 	credentials: "include"
+			// }).then(res => {})
 		}
 	}
 
